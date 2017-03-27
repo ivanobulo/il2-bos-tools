620688dfad3983f5de5b3eab35bb06577200256a
Ivan Luzyanin <ivanobulo@gmail.com>
2017-03-26 17:24:46 -0700

    test against large downloaded files

--- txtlog-processor/src/test/scala/org/github/il2boscontrol/txtlog/ProcessAll.scala
+++ txtlog-processor/src/test/scala/org/github/il2boscontrol/txtlog/ProcessAll.scala
@@ -7,27 +7,29 @@ import java.util.function.Consumer
 
 import scala.util.{Failure, Success, Try}
 
-/**
-  * Created by ivano on 3/26/2017.
-  */
 object ProcessAll extends App {
 
-  val files = Files.newDirectoryStream(new File("C:\\Users\\ivano.DESKTOP-7AGOO0J\\Downloads\\logs\\txt").toPath)
+  val files = Files.newDirectoryStream(new File("C:\\Users\\ivano.DESKTOP-7AGOO0J\\Downloads\\logs\\txt\\mission_report_backup\\2016\\10\\6").toPath)
 
   files.forEach(new Consumer[Path] {
     override def accept(t: Path) = {
-      val reader = new LineNumberReader(Files.newBufferedReader(t))
+      try {
 
-      val source = scala.io.Source.fromFile(t.toFile)
-      val events = source.getLines.map{ line =>
-        line -> EventlogEventParsers.parse(EventlogEventParsers.event, line)
-      }
-      events.foreach {
-        case (line, r) if r.successful => //println(r.get)
-        case (line, r) => println(s"err: $line, $r")
-        case _ =>
-      }
+        val reader = new LineNumberReader(Files.newBufferedReader(t))
 
+        println(s"Reading file $t")
+
+        val source = scala.io.Source.fromFile(t.toFile)
+        val events = source.getLines.map { line =>
+          line -> EventlogEventParsers.parse(EventlogEventParsers.event, line)
+        }
+        events.foreach {
+          case (line, r) if r.successful => //println(r.get)
+          case (line, r) => println(s"err: $line, $r")
+        }
+      } catch {
+        case ex => println(ex)
+      }
     }
   })
 
